74f58a85a81617600e2a58b5c7449bd6e3bef4bf
alyssa ruiz <alyssaruiz@quickenloans.com>
2019-07-24 09:52:36 -0700

    this is my third repo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getbranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld.");
+
+            for (int i = 1; i < 11; i++)
+                Console.WriteLine(i);
+            Console.ReadKey();
+
+        }
+        
+
+    }
+}
